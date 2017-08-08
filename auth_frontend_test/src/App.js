169c5772f3d7fabb6265a8b8ccd35f6f4a78371c
Stefan Hartmann <stefanhartmann@gmail.com>
2017-08-08 13:18:35 -0400

    get set up to add j-toker

--- auth_frontend_test/src/App.js
+++ auth_frontend_test/src/App.js
@@ -7,23 +7,49 @@ class App extends Component {
     super(); 
 
     this.state = {
-      uid: null
+      uid: null,
+      email: "Enter your email here!",
+      password: ""
     };
-
+    
+    this.changeHandler = this.changeHandler.bind(this);
+    this.submitForm = this.submitForm.bind(this);
   };
-  checkUserAuth(userStatus) {
-    if (userStatus.uid === null) {
+  changeHandler(e) {
+    if(e.target.name === "email_address"){
+      this.setState({email: e.target.value})
+    } else if (e.target.name === "password") {
+      this.setState({password: e.target.value})
+    };
+  };
+  submitForm() {
+  
+  };
+  checkUserAuth(uid) {
+    if (uid === null) {
       return(
-        <form>
+        <form onSubmit={this.checkAuth}>
             Email Address: <br/>
-            <input type="text" name="email_address"/><br/>
+            <input type="text" name="email_address" value={this.state.email} onChange={this.changeHandler}/><br/>
             Password: <br/>
-            <input type="password" name="password"/><br/>
+            <input type="password" name="password" value={this.state.password} onChange={this.changeHandler}/><br/>
             <input type="submit"/>
         </form>
       )
     } else {
-      
+      return (
+        <div>
+          <h3>I'm in.</h3>
+          <iframe 
+            width="853" 
+            height="480" 
+            src="https://www.youtube.com/embed/gXUVqLxNI6c?start=7&autoplay=1" 
+            frameBorder="0" 
+            title="Successfully Authorized"
+            allowFullScreen>
+          </iframe>
+        </div>
+      )
     }
   }
   render() {
@@ -31,10 +57,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to React + Rails Token Based Auth</h2>
         </div>
         <div>
-          { this.checkUserAuth(this.state) }
+          { this.checkUserAuth(this.state.uid) }
         </div>
       </div>
     );
